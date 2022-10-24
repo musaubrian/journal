da5e276c1faa3df8c3936c578610e3ee70729c49
musaubrian <musaubrian15@gmail.com>
2022-10-24 16:59:46 +0300

    Chore: retrieve data successfully

--- journal-vue/src/views/HomePageView.vue
+++ journal-vue/src/views/HomePageView.vue
@@ -1,8 +1,31 @@
 <template>
     <div class="homepage">
         <h1>homepage</h1>
+        <button @click="fetchEntries">get data</button>
     </div>
 </template>
+
+<script setup>
+import { supabase } from "../supabase";
+import { ref } from "vue";
+
+// const [fetchError, setFetchError] = ref(null)
+// const [journalEntries, setJournalEntries] = ref(null)
+
+const fetchEntries = async () => {
+    const {data, error} = await supabase
+    .from('journal_entries')
+    .select()
+
+    if (error) {
+        console.log(error)
+    }
+    if (data) {
+        console.log(data)
+    }
+
+}
+</script>
 
 <style scoped>
 .homepage {
